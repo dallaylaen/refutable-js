170cc3adc976ab33bef1ece2643d06907c92bc9c
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-18 07:45:06 +0200

    [speed] Add speedtest, no need to care about performance rn

---
+++ speed.js
@@ -0,0 +1,15 @@
+const refute = require('./lib/refute.js');
+
+// TODO normal args & timing
+// use time js speed.js
+
+for (let i = 0; i < 1000; i++) {
+    refute( ok => {
+        for (let j = 0; j < 1000; j++) {
+            ok.equals( j, j, 'j equals itself' );
+        };
+        ok.getReport();
+    });
+    
+};
+
