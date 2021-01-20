13e17d9b78494e4a7d979b867484ebe85e0809a1
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-20 06:01:43 +0200

    [test] Slightly reorg test

--- test/401-bad-arg-count.js
+++ test/401-bad-arg-count.js
@@ -6,9 +6,12 @@ const expect = chai.expect;
 const refute = require( '../lib/refute.js' );
 
 describe( 'Report.errors', () => {
-    it( 'detects arg count', done => {
-        const ok = new refute.Report();
+    const ok = new refute.Report();
+    it( 'detects missing arguments', done => {
         expect( _ => ok.equals( 1 ) ).to.throw(/Bad argument/);
+        done();
+    });
+    it( 'detects extra arguments', done => {
         expect( _ => ok.equals( 1, 1, 1, 'some test' ) ).to.throw(/Bad argument/);
         done();
     });
