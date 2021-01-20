c16ce4ebde6f4c7e9c035c61e9468f2c79bed4ee
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-20 19:33:42 +0200

    [arsenal] Add map(name, array, contract) check

---
+++ test/205-map.js
@@ -0,0 +1,22 @@
+'use strict';
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+
+const refute = require( '../lib/refute.js' );
+
+describe( 'Report.map', () => {
+    it ( 'runs subtest', done => {
+        const outer = new refute.Report();
+        outer.map('some array', [ 'foo42', 'bar137', 'foobar' ], (ok, item) => {
+            ok.matches( item, /(.)\1/ );
+            ok.matches( item, /[a-z]+[0-9]+/ );
+        });
+        outer.stop();
+
+        console.log( outer.getTap() );
+        expect( outer.getGhost() ).to.equal('r(r(r(2),r(N,1),r(1,N)))');
+
+        done();
+    });
+});
