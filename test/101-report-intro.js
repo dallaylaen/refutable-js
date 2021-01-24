896c8ce7860e1b1347e7b503d9e8f04c57e84b4f
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-25 00:45:47 +0200

    [test] Add proper check for check() primitive

---
+++ test/101-report-intro.js
@@ -0,0 +1,34 @@
+'use strict';
+const { expect } = require('chai');
+
+const { Report } = require( '../lib/refute/report.js' );
+
+describe( 'Report', () => {
+    it ('can execute checks', done => {
+        const ok = new Report();
+        expect( ok.isDone()    ).to.equal( false );
+        expect( ok.getCount()  ).to.equal( 0 );
+
+        expect( ok.check( 0, 'zero evidence check' ) ).to.equal( true );
+        expect( ok.isDone()    ).to.equal( false );
+        expect( ok.getCount()  ).to.equal( 1 );
+        expect( ok.isPassing() ).to.equal( true );
+        
+        expect( ok.check( 'objection!', 'Phoenix Wright appears' ) ).to.equal( false );
+        expect( ok.isDone()    ).to.equal( false );
+        expect( ok.getCount()  ).to.equal( 2 );
+        expect( ok.isPassing() ).to.equal( false );
+
+        // check chainability
+        expect( ok.diag( 'some text' ) ).to.equal( ok );
+        expect( ok.stop() ).to.equal( ok );
+
+        expect( ok.isDone()    ).to.equal( true );
+        expect( ok.getCount()  ).to.equal( 2 );
+        expect( ok.isPassing() ).to.equal( false );
+        expect( ok.getGhost()  ).to.equal( 'r(1,N)' );
+
+        done();
+    });
+        
+});
