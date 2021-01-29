42e8acace7d239bd4704668eff60c8e4eec7adbc
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-29 18:40:15 +0200

    [test] Add detailed getDetails test

---
+++ test/113-get-details.js
@@ -0,0 +1,50 @@
+'use strict';
+const { expect } = require('chai');
+
+const { Report } = require( '../lib/refute/report.js' );
+
+describe( 'Report.getDetails(n)', () => {
+    const ok = new Report();
+    ok.diag( 'some contract' );
+    ok.check( '', 'passing check' );
+    ok.check( {foo:42}, 'failing check' );
+    ok.diag( 'see?' );
+    ok.nested( 'nested check', inner => inner.pass() );
+
+    it ('n=0', done => {
+        const data = ok.getDetails(0);
+        expect( data.n ).to.equal( 0 );
+        expect( data.diag ).to.deep.equal( [ 'some contract' ] );
+        done();
+    });
+
+    it ('n=1', done => {
+        const data = ok.getDetails(1);
+        expect( data.n ).to.equal( 1 );
+        expect( data.name ).to.equal( 'passing check' );
+        expect( data.pass ).to.equal( true );
+        // TODO evidence should be missing, now it's empty array
+        expect( data.diag ).to.deep.equal( [ ] );
+        done();
+    });
+    
+    it ('n=2', done => {
+        const data = ok.getDetails(2);
+        expect( data.n ).to.equal( 2 );
+        expect( data.name ).to.equal( 'failing check' );
+        expect( data.pass ).to.equal( false );
+        expect( data.evidence ).to.deep.equal( [ '{"foo":42}' ] );
+        expect( data.diag ).to.deep.equal( [ 'see?' ] );
+        done();
+    });
+
+    it ('n=3', done => {
+        const data = ok.getDetails(3);
+        expect( data.n ).to.equal( 3 );
+        expect( data.name ).to.equal( 'nested check' );
+        expect( data.pass ).to.equal( true );
+        expect( data.nested ).to.be.instanceof( Report );
+        done();
+    });
+
+});
