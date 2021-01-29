bc584eadd1eba0e1d16d5470ca287c71ab109270
Konstantin S. Uvarin <khedin@gmail.com>
2021-01-29 17:42:36 +0200

    [report] Add toJSON

---
+++ test/210-to-json.js
@@ -0,0 +1,37 @@
+'use strict';
+const { expect } = require('chai');
+
+const refute = require( '../lib/refute.js' );
+const { Report } = refute;
+
+describe( 'Report', () => {
+    const jsonContract = (ok, report, json) => {
+        console.log(ok);
+        ok.type( json, 'object', 'json is object' );
+
+        // scalar fields
+        ok.equal( json.pass, report.isPassing(), 'pass is reflected' );
+        ok.equal( json.count, report.getCount(), 'count is reflected' );
+
+        // details
+        ok.type( json.details, 'array', 'details is array' );
+        ok.equal( json.details.length, json.count+1, 'detail length is aligned with count' );
+        // TODO validate details content via ok.map()
+    };
+
+    it ('can toJSON', done => {
+        const report = new Report().run( ok => {
+            ok.check( '', 'passing test' );
+            ok.check( { foo: 42}, 'failing test' );
+            ok.diag( "foo bared", { bar: 137 } );
+        }).stop();
+
+        const str = JSON.stringify(report);
+
+        const raw = JSON.parse(str);
+
+        refute( report, raw, jsonContract );
+
+        done();
+    });
+});
