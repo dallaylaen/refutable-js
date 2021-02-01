a41d36598ca0fb7532c16ca4a8360b9c5a31eaf2
Konstantin S. Uvarin <khedin@gmail.com>
2021-02-01 02:08:15 +0200

    [test] Add addCondition bad arguments test

---
+++ test/402-add-condition-wrong.js
@@ -0,0 +1,22 @@
+'use strict';
+const { expect } = require( 'chai' );
+const { addCondition } = require( '../lib/refute.js' );
+
+describe( 'addCondition errors', () => {
+    it( 'forbids bad args', done => {
+        expect( _ => addCondition( 'foo', {args:1}, 42 ) )
+            .to.throw(/impl/);
+        expect( _ => addCondition( 42, {args:1}, x=>x ) )
+            .to.throw(/must.*string/);
+        expect( _ => addCondition( 'foo', 'args:1', x=>x ) )
+            .to.throw(/options/);
+        expect( _ => addCondition( 'foo', {}, x=>x ) )
+            .to.throw(/[Aa]rgs.*number/);
+        done();
+    });
+    it( 'does not override Report methods', done => {
+        expect( _ => addCondition( 'toJSON', {args:1}, x=>x ) )
+            .to.throw(/[Mmethod].*toJSON/);
+        done();
+    });
+});
