6bc423b1d64edd6edabc12c852e65aeb25c827b2
Konstantin S. Uvarin <khedin@gmail.com>
2021-02-02 00:08:14 +0200

    [report] Add last() to get last check success, getPass(n) to get any check success

--- test/101-report-intro.js
+++ test/101-report-intro.js
@@ -9,16 +9,19 @@ describe( 'Report', () => {
         expect( ok.getDone()    ).to.equal( false );
         expect( ok.getCount()  ).to.equal( 0 );
         expect( ok.getPass() ).to.equal( true );
+        expect( ok.last() ).to.equal( undefined );
 
         expect( ok.check( 0, 'zero evidence check' ) ).to.equal( undefined );
         expect( ok.getDone()    ).to.equal( false );
         expect( ok.getCount()  ).to.equal( 1 );
         expect( ok.getPass() ).to.equal( true );
+        expect( ok.last() ).to.equal( true );
         
         expect( ok.check( 'objection!', 'Phoenix Wright appears' ) ).to.equal( undefined );
         expect( ok.getDone()    ).to.equal( false );
         expect( ok.getCount()  ).to.equal( 2 );
         expect( ok.getPass() ).to.equal( false );
+        expect( ok.last() ).to.equal( false );
 
         // check chainability
         expect( ok.diag( 'some text' ) ).to.equal( ok );
@@ -31,6 +34,14 @@ describe( 'Report', () => {
 
         expect( () => ok.diag('more text') ).to.throw(/finished/);
         expect( () => ok.check(0, 'extra check') ).to.throw(/finished/);
+
+        // unaffected by dying calls
+        expect( ok.getCount() ).to.equal( 2 );
+
+        expect( ok.getPass(0) ).to.equal( undefined );
+        expect( ok.getPass(1) ).to.equal( true );
+        expect( ok.getPass(2) ).to.equal( false );
+        expect( ok.getPass(3) ).to.equal( undefined );
 
         done();
     });
