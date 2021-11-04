04117f89c78fe0829d134f29abad4b168f3c972a
Konstantin S. Uvarin <khedin@gmail.com>
2021-11-04 07:22:10 +0200

    [difftree] Trying to find a suitable TAP-like format

--- test/diff-tree/001-usage.js
+++ test/diff-tree/001-usage.js
@@ -4,6 +4,14 @@ const { expect } = require( 'chai' );
 
 const diffTree = require( '../../lib/diff-tree.js' );
 
+// TODO do we need it in diffTree itself?
+function embed( ...messages) {
+    const log = diffTree.log();
+    for( let i of messages )
+        log.append(i);
+    return log;
+}
+
 describe( 'DiffTree', () => {
     it( 'can append lines & render', done => {
         const tree = diffTree.log();
@@ -16,6 +24,33 @@ describe( 'DiffTree', () => {
         const render = tree.render();
 
         expect( render ).to.equal( '- war\n+ peace\n' );
+
+        done();
+    });
+
+    it( 'can do a tap-like render', done => {
+        const tree = diffTree.log();
+
+        tree.append( diffTree.plan( '1..3' ) );
+        tree.append( diffTree.pass( '1 - passing test' ) );
+
+        const inner = diffTree.log();
+        inner.append( diffTree.plan( '1..2' ) );
+        inner.append( diffTree.fail( '1 - failing test' ) );
+        inner.append( embed(
+            diffTree.actual( 'war' ),
+            diffTree.expected( 'peace' ),
+            diffTree.note( 'we are not in 1984' )
+        ));
+        inner.append( diffTree.pass( '2' ) );
+
+        tree.append( diffTree.fail( '2 (subtest) - war & peace' ) );
+        tree.append( inner );
+
+        tree.append( diffTree.pass( '3 - another passing test' ) );
+        tree.append( diffTree.note( 'that\'s all folks' ) );
+
+        console.log( tree.render() );
 
         done();
     });
