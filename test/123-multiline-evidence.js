cc2206a706fd5c17bef7b251104e19b68e48dc48
Konstantin S. Uvarin <khedin@gmail.com>
2021-12-03 11:19:30 +0200

    [bug] Add multiline evidence support

---
+++ test/123-multiline-evidence.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { expect } = require( 'chai' );
+const { Report } = require( '../lib/refute/report.js' );
+
+describe( 'Report.getText', () => {
+    it( 'hanles multiline evidence', done => {
+        const rep = new Report();
+        rep.check( 'foo\nbar\nbaz\n' );
+        const text = rep.getText();
+
+        expect( text ).to.match(
+            new RegExp("\n +\\^ foo\n +\\^ bar\n +\\^ baz\n[)]") );
+
+
+        done();
+    });
+
+    it( 'hanles multiline diff evidence', done => {
+        const rep = new Report();
+        rep.check( ['- foo\nbar', '+ bar\nfoo' ] );
+        const text = rep.getText();
+
+        expect( text ).to.match(
+            new RegExp("\n +\\- foo\n +\\- bar\n +\\+ bar\n +\\+ foo\n[)]") );
+
+
+        done();
+    });
+});
