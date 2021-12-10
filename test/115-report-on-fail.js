28bfefd2c3aae1217f9cb586d1373ca223540008
Konstantin S. Uvarin <khedin@gmail.com>
2021-12-10 18:59:44 +0200

    [api] Add onFail(fn) callback

---
+++ test/115-report-on-fail.js
@@ -0,0 +1,25 @@
+'use strict';
+const { expect } = require( 'chai' );
+const { Report } = require( '../lib/refute/report.js' );
+
+describe( 'Report.onFail()', () => {
+    const preset = list => new Report()
+        .onDone( x=>list.push(1) )
+        .onFail( x=>list.push(x) )
+        .onDone( x=>list.push(3) );
+
+    it( 'can call onFail callback', done => {
+        const trace = [];
+        const rep = preset(trace).run( ok => ok.fail() );
+        expect( trace ).to.deep.equal( [3,rep,1] );
+        done();
+    });
+
+    it( 'can skip onFail callback', done => {
+        const trace = [];
+        const rep = preset(trace).run( ok => ok.pass() );
+        expect( trace ).to.deep.equal( [3,1] );
+        done();
+    });
+    // TODO async
+});
