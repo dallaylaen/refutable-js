6a8028b0ab8dce57603b4adcc5464fca34cdd9ce
Konstantin S. Uvarin <khedin@gmail.com>
2021-12-13 03:21:59 +0200

    [example] Add some homegrown options to speed.js

--- example/speed.js
+++ example/speed.js
@@ -1,20 +1,38 @@
 'use strict';
-const refute = require('./lib/refute.js');
+const refute = require('../lib/refute.js')
+    .config({ onFail: skip=>true });
 
 // TODO use some kind of getopt
-const runs   = Number.parseInt(process.argv[2]) || 1000;
-const checks = Number.parseInt(process.argv[3]) || 1000;
+const args = pargs(process.argv.slice(2));
+
+const runs   = args.runs   || 1000;
+const checks = args.checks || 1000;
+const fail   = args.fail   || 0;
+const print  = args.print  ?? 1;
 
 const t0 = new Date();
 for (let i = 0; i < runs; i++) {
     refute( ok => {
+        let s = 0;
         for (let j = 0; j < checks; j++) {
-            ok.match( j, /\d+/, 'j is a number' );
+            ok.cmpNum( s, '==', j*j + fail, 'square' );
+            s += 2*j+1;
         };
-        ok.toString();
+        if(print)
+            ok.toString();
     });
 };
 
 const milli = new Date() - t0;
 console.log( 'Done '+runs+' runs of '+checks+' checks each in '+milli+'ms' );
 console.log( (runs*checks/milli).toFixed(2) + ' checks/ms' );
+
+function pargs (list) {
+    const out = {};
+    list.forEach(s=>{
+        const [ _, name, value ] = s.match(/(\w+)=(.*)/);
+        const maybe = Number.parseInt(value);
+        out[name] = Number.isNaN(maybe) ? value : maybe;
+    });
+    return out;
+}
