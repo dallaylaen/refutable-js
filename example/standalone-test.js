f14310de450bd273ce869048812c55b51aed3602
Konstantin S. Uvarin <khedin@gmail.com>
2021-12-13 03:21:24 +0200

    [example] Fix path in example

--- example/standalone-test.js
+++ example/standalone-test.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const { refuteUnit } = require( './lib/refute/mocha.js' );
+const { refuteUnit } = require( '../lib/refute/mocha.js' );
 
 refuteUnit( "Foo bar", ok => {
     ok.equal( 42, 137, 'life is fine' );
     ok.deepEqual( [1, 3, 7, {foo:1}], [1, 3, 8, {foo:2}], 'complex evd' );
     ok.ordered( 'even more complex evd',
          [1, 4, 16, 15, 30, 60],
-         (r,x,y) => r.numCmp( x, '<=', y, 'increasing values' ) );
+         (r,x,y) => r.cmpNum( x, '<=', y, 'increasing values' ) );
 
 });
