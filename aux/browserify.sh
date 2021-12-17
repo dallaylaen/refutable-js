2ee635173b8a9102395f6e92cddf35a51c774e99
Konstantin S. Uvarin <khedin@gmail.com>
2021-12-17 04:20:10 +0200

    [aux] Rename index.js to web-index.js

--- aux/browserify.sh
+++ aux/browserify.sh
@@ -2,11 +2,12 @@
 
 set -ex
 
-JS=docs/js
+SRC=./lib/web-index.js
+DST=docs/js
 
-webpack-cli ./lib/index.js --mode development -o $JS
-mv $JS/main.js $JS/refute.js
+webpack-cli "$SRC" --mode development -o "$DST"
+mv "$DST"/main.js "$DST"/refute.js
 
-webpack-cli ./lib/index.js --mode production -o $JS
-mv $JS/main.js $JS/refute.min.js
+webpack-cli "$SRC" --mode production -o "$DST"
+mv "$DST"/main.js "$DST"/refute.min.js
 
