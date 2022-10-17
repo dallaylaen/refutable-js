8835996ed7534248ef7b5688d45fa71238877acf
Konstantin S. Uvarin <khedin@gmail.com>
2022-10-17 13:44:17 +0200

    [aux] Small cleanup in scripts

--- aux/browserify.sh
+++ aux/browserify.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
 SRC=./lib/web-index.js
 DST=docs/js
 
 webpack-cli "$SRC" --mode development -o "$DST"
-mv "$DST"/main.js "$DST"/refute.js
+mv -f "$DST"/main.js "$DST"/refute.js
 
 webpack-cli "$SRC" --mode production -o "$DST"
-mv "$DST"/main.js "$DST"/refute.min.js
+mv -f "$DST"/main.js "$DST"/refute.min.js
 
