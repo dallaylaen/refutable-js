ac47bc97392fb3693fe79474aadf2f5c0f05bafc
Konstantin S. Uvarin <khedin@gmail.com>
2022-10-26 16:33:58 +0200

    [repo] Go back from webpack to browserify

--- aux/browserify.sh
+++ aux/browserify.sh
@@ -5,9 +5,7 @@ set -eux
 SRC=./lib/web-index.js
 DST=docs/js
 
-webpack-cli "$SRC" --mode development -o "$DST"
-mv -f "$DST"/main.js "$DST"/refute.js
+browserify "$SRC" -d -o "$DST"/refute.js
 
-webpack-cli "$SRC" --mode production -o "$DST"
-mv -f "$DST"/main.js "$DST"/refute.min.js
+browserify "$SRC" -p tinyify -o "$DST"/refute.min.js
 
